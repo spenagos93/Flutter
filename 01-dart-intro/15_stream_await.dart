b2baf40531a24066646ba54f147c56eb9a0c5ac3
spenagos93 <santipenagos93@gmail.com>
2023-05-24 08:31:39 -0500

    se agrega 15_stream_await

---
+++ 01-dart-intro/15_stream_await.dart
@@ -0,0 +1,14 @@
+void main() {
+  emitNumbers().listen((int value) {
+    print('Stream value: $value');
+  });
+}
+
+Stream<int> emitNumbers() async* {
+  final valuesToEmit = [1, 2, 3, 4, 5];
+
+  for (int i in valuesToEmit) {
+    await Future.delayed(const Duration(seconds: 1));
+    yield i;
+  }
+}
